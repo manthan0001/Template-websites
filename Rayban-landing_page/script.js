71e51a8dd340d4073be21269fc5db5c32ce0d9af
manthan0001 <manthanp048@gmail.com>
2023-10-18 23:59:05 +0530

    Added sheryjs script and completed web effect

---
+++ Rayban-landing_page/script.js
@@ -0,0 +1,34 @@
+Shery.imageEffect("#back", {
+    style: 5,
+    config: {
+        "a":{"value":1,"range":[0,30]},
+        "b":{"value":0.98,"range":[-1,1]},
+        "zindex":{"value":-9996999,"range":[-9999999,9999999]},
+        "aspect":{"value":2.1286031362206517},
+        "gooey":{"value":true},
+        "infiniteGooey":{"value":true},
+        "growSize":{"value":4,
+        "range":[1,15]},
+        "durationOut":{"value":1,
+        "range":[0.1,5]},
+        "durationIn":{"value":1,"range":[0.1,5]},
+        "displaceAmount":{"value":0.5},
+        "masker":{"value":true},
+        "maskVal":{"value":1.1,
+        "range":[1,5]},
+        "scrollType":{"value":0},
+        "geoVertex":{"range":[1,64],"value":1},
+        "noEffectGooey":{"value":false},
+        "onMouse":{"value":1},
+        "noise_speed":{"value":0.74,
+        "range":[0,10]},
+        "metaball":{"value":0.2,
+        "range":[0,2],
+        "_gsap":{"id":3}},
+        "discard_threshold":{"value":0.5,"range":[0,1]},
+        "antialias_threshold":{"value":0,"range":[0,0.1]},
+        "noise_height":{"value":0.3,"range":[0,2]},
+        "noise_scale":{"value":21.49,"range":[0,100]}}
+
+   
+     ,gooey: true});
